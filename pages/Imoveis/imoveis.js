a70ac3119425852f7dbbada22646332f75f54c93
Richard Orlandini de Souza Aleixo <t02939@complexo.unimed>
2022-11-06 18:30:26 -0300

    Criando arquivo js para aclopar os cards.

---
+++ pages/Imoveis/imoveis.js
@@ -0,0 +1,35 @@
+
+
+function createImg(src, alt){
+    return `
+        <img
+        src="/imgs/${src}.jpg" 
+        alt=${alt}>
+    `
+}
+function createCard(spanOne,spanTwo, imgs ){
+    return `
+    <div class="card"> 
+     ${imgs}
+    <h2><span>${spanOne}</span>${spanTwo}</h2>
+    </div>
+    `
+
+}
+
+document.querySelector("#container").innerHTML = `
+    ${createCard("Ver mais", "São paulo",
+    createImg("img1", "Imagem de uma casa Chique"
+    ))}
+    ${createCard("Ver mais", "São paulo",
+    createImg("img1", "Imagem de uma casa Chique"
+    ))}
+    ${createCard("Ver mais", "São paulo",
+    createImg("img1", "Imagem de uma casa Chique"
+    ))}
+    ${createCard("Ver mais", "São paulo",
+    createImg("img1", "Imagem de uma casa Chique"
+    ))}
+    
+
+`
