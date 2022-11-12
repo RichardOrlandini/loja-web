26396a35b57bdd38966bcc6ea953bcdfa6af0d68
Richard Orlandini de Souza Aleixo <t02939@complexo.unimed>
2022-11-12 15:41:15 -0300

    Correção arquivo imoveis

--- src/imoveis.js
+++ src/imoveis.js
@@ -1,52 +1,57 @@
- function createImg(src, alt){
-    return `
-    <img
-    src="/imgs/${src}.jpg"
-    alt=${alt}>
+
+
+function createImg(src, alt){
+   return `
+       <img
+       src="/imgs/${src}.jpg"  
+       alt=${alt}>
+       
    `
-    }
+}
+// crie uma função:
+/*
+Retorne uma imagem e coloque uma descrição
+*/
+function createCard(spanOne,spanTwo, imgs ){
+   return `
+   <div class="card"> 
+    ${imgs}
+   <h2><span>${spanOne}</span>${spanTwo}</h2>
+   </div>
+   `
 
-    // crie uma função:
-    /*
-    Retorne uma imagem e coloque uma descrição
-    */
+}
+
+document.querySelector("#container").innerHTML = `
+   ${createCard("Ver mais", "São paulo",
+   createImg("img1", "Imagem de uma casa Chique"
+   ))}
+   ${createCard("Ver mais", "São paulo",
+   createImg("img2", "Imagem de uma casa Chique"
+   ))}
+   ${createCard("Ver mais", "São paulo",
+   createImg("img3", "Imagem de uma casa Chique"
+   ))}
+   ${createCard("Ver mais", "São paulo",
+   createImg("img4", "Imagem de uma casa Chique"
+   ))}
    
-    function createCard(spanOne,spanTwo, imgs ){
-    return     `
-    <div class="card">${imgs}
-    <h2><span>${spanOne}</span>${spanTwo}</h2>
-    </div>`
-    }
-    
-    document.querySelector("#container").innerHTML = `
-    ${createCard("Ver mais", "São paulo",
-    createImg("img1", "Imagem de uma casa Chique"
-    ))}
-    ${createCard("Ver mais", "São paulo",
-    createImg("img2", "Imagem de uma casa Chique"
-    ))}
-    ${createCard("Ver mais", "São paulo",
-    createImg("img3", "Imagem de uma casa Chique"
-    ))}
-    ${createCard("Ver mais", "São paulo",
-    createImg("img4", "Imagem de uma casa Chique"
-    ))}
-    `
 
-    document.querySelector("#container2").innerHTML =
-    `
-    ${createCard("Ver mais", "São paulo",
-    createImg("img1", "Imagem de uma casa Chique"
-    ))}
-    ${createCard("Ver mais", "São paulo",
-    createImg("img2", "Imagem de uma casa Chique"
-    ))}
-    ${createCard("Ver mais", "São paulo",
-    createImg("img3", "Imagem de uma casa Chique"
-    ))}
-    ${createCard("Ver mais", "São paulo",
-    createImg("img4", "Imagem de uma casa Chique"
-    ))}
-    `
-    
-    const clickSpan = document.querySelector("span")
+`
+document.querySelector("#container2").innerHTML = `
+   ${createCard("Ver mais", "São paulo",
+   createImg("img1", "Imagem de uma casa Chique"
+   ))}
+   ${createCard("Ver mais", "São paulo",
+   createImg("img2", "Imagem de uma casa Chique"
+   ))}
+   ${createCard("Ver mais", "São paulo",
+   createImg("img3", "Imagem de uma casa Chique"
+   ))}
+   ${createCard("Ver mais", "São paulo",
+   createImg("img4", "Imagem de uma casa Chique"
+   ))}
+`
+
+
+const clickSpan = document.querySelector("span")
