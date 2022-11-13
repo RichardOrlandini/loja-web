994125114e5a76b1b88871095447cfc0ec2f9dc6
Richard Orlandini de Souza Aleixo <t02939@complexo.unimed>
2022-11-13 09:35:40 -0300

    Ajustando responsividade da pagina Home

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import  {Router} from './router.js'
+//import  {createImg, createCard} from './imoveis'
 
 const router =  new Router()
 router.add("/", "/pages/home.html")
@@ -12,3 +13,13 @@ router.handle()
 
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
+
+//Pagina de Imoveis
+
+/*
+
+import { CardView } from './Card.js'
+
+new CardView("app")
+
+*/
