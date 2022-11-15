384fb3505cf5258d9699caefbda92b5daa23698c
Richard Orlandini de Souza Aleixo <t02939@complexo.unimed>
2022-11-15 18:35:37 -0300

    Correções

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import  {Router} from './router.js'
-//import  {createImg, createCard} from './imoveis'
+
 
 const router =  new Router()
 router.add("/", "/pages/home.html")
@@ -14,12 +14,4 @@ router.handle()
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
 
-//Pagina de Imoveis
 
-/*
-
-import { CardView } from './Card.js'
-
-new CardView("app")
-
-*/
